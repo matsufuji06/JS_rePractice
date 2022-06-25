e5b2edb5549971569303655d9c2e76d8fbd924e5
matsufuji06 <n.matsufuji06@gmail.com>
2022-06-25 11:21:05 +0900

    テキストボックスの値を取得し、初期化する

--- jakee/js_app/main.js
+++ jakee/js_app/main.js
@@ -1,2 +1,11 @@
-import "css/style.css";
+// import "css/style.css";
 
+const onClickAdd = () => {
+  // テキストボックスの値を取得し、初期化する
+  // alert("追加ボタンが押されました");
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+  alert(inputText);
+}
+
+document.getElementById("add-button").addEventListener("click", () => onClickAdd());
