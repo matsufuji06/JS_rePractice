7729382811120dc35da46f44fb268e8b460eaa31
matsufuji06 <n.matsufuji06@gmail.com>
2022-06-25 11:41:38 +0900

    タスクのテキスト追加機能を実践

--- jakee/js_app/main.js
+++ jakee/js_app/main.js
@@ -1,11 +1,25 @@
 // import "css/style.css";
 
 const onClickAdd = () => {
-  // テキストボックスの値を取得し、初期化する
-  // alert("追加ボタンが押されました");
+  // テキストボックスの値を取得し、初期化
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
-  alert(inputText);
+
+  // divを生成し、classNameでクラス名を付与
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  // liタグを生成し、innerTextでテキストを付与
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  // divタグの子要素として、liタグを設定
+  div.appendChild(li);
+
+  
+  // 未完了のリストに追加
+  document.getElementById("incomplete-list").appendChild(div);
+
 }
 
 document.getElementById("add-button").addEventListener("click", () => onClickAdd());
