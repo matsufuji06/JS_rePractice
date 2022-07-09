12702354134bc13f44a1c941d77da9504eeb1f56
matsufuji06 <n.matsufuji06@gmail.com>
2022-07-09 16:14:51 +0900

    本01の練習終了

---
+++ jakee/book/01/src/index.js
@@ -0,0 +1,32 @@
+// const title1 = document.getElementById("title");
+// console.log(title1);
+
+// const title2 = document.querySelector("#title");
+// console.log(title2);
+
+// --------------------
+
+// const containers = document.querySelectorAll(".container");
+// console.log(containers);
+
+// --------------------
+
+// const divEl = document.createElement("div");
+// const pEl = document.createElement("p");
+
+// divEl.appendChild(pEl);
+// console.log(divEl);
+
+// --------------------
+
+const buttonEl = document.createElement("button");
+
+buttonEl.textContent = "ボタン";
+console.log(buttonEl);
+
+const divEl = document.querySelector(".container");
+
+divEl.appendChild(buttonEl);
+
+
+// --------------------
